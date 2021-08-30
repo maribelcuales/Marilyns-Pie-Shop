e9d14ffe58dd88d2f3436c497c20241f6819cdda
maribelcuales <maribelcuales@gmail.com>
2021-08-30 17:02:45 +0800

    Create server to listen on port 5000

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ router.get('/', function(req, res, next) {
 
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router); 
+
+// Create server to listen on port 5000
+var server = app.listen(5000, function () {
+  console.log('Node server is running on http://localhost:5000..');
+});
