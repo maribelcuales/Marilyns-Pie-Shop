48a9e10b2451330ffb47633b2f69b6bd63ed4af5
maribelcuales <maribelcuales@gmail.com>
2021-08-31 17:14:32 +0800

    Update pieRepo get function to read from created pies data file.

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -1,12 +1,18 @@
-// Create module for pie data 
+let fs = require('fs');
+
+const FILE_NAME = './assets/pies.json';
+
 let pieRepo = {
-  get: function() {
-    return [
-      { "id": 1, "name": "Apple" },
-      { "id": 2, "name": "Cherry" },
-      { "id": 3, "name": "Peach" },
-    ];
+  get: function(resolve, reject) {
+    fs.readFile(FILE_NAME, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(JSON.parse(data));
+      }
+    });
   }
-};
+} 
 
 module.exports = pieRepo; 
