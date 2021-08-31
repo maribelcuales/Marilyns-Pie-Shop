19c6ed64bcf3d9c644c1327b351072f048a72304
maribelcuales <maribelcuales@gmail.com>
2021-08-31 20:30:53 +0800

    Add getById method to pieRepo.

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -12,6 +12,17 @@ let pieRepo = {
         resolve(JSON.parse(data));
       }
     });
+  },
+  getById: function(id, resolve, reject) {
+    fs.readFile(FILE_NAME, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let pie = JSON.parse(data).find(p => p.id == id);
+        resolve(pie);
+      }
+    });
   }
 } 
 
