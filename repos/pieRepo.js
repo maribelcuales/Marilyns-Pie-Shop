a4a27830def6cb27ad735d906561526096916a97
maribelcuales <maribelcuales@gmail.com>
2021-09-01 17:22:50 +0800

    Add Search method.

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -23,7 +23,30 @@ let pieRepo = {
         resolve(pie);
       }
     });
+  },
+  search: function (searchObject, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let pies = JSON.parse(data);
+        //Perform search 
+        if (searchObject) {
+          //  Example search object 
+          //  let searchObject = {
+          //    "id": 1, 
+          //    "name": 'A'
+          //  };
+          pies = pies.filter(
+            p => (searchObject.id ? p.id == searchObject.id : true) && 
+              (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowercase()) >= 0 : true));
+        }
+
+        resolve(pies);
+      }
+    });
   }
-} 
+}
 
 module.exports = pieRepo; 
