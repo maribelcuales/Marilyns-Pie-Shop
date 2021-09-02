490ef28a59f5f9e9978134cec230c1e3566a4c0f
maribelcuales <maribelcuales@gmail.com>
2021-09-02 21:03:35 +0800

    Added patch method to partially update data.

--- index.js
+++ index.js
@@ -140,6 +140,35 @@ router.delete('/:id', function (req, res, next) {
   });
 })
 
+router.patch('/:id', function (req, res, next) {
+  pieRepo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to update the data
+      pieRepo.update(req.body, req.params.id, function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "Pie '" + req.params.id + "' patched.",
+          "data": data
+        });
+      });
+    }
+    else {
+      res.status(404).json({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The pie '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The pie '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  });
+})
+
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router); 
 
