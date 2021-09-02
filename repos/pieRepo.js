caba3b427a0826808c0468fd4f2e92f98fcd1625
maribelcuales <maribelcuales@gmail.com>
2021-09-02 19:59:02 +0800

    Add delete function to pieRepo.

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -87,6 +87,28 @@ let pieRepo = {
         }
       }
     });
+  },
+  delete: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let pies = JSON.parse(data);
+        let index = pies.findIndex(p => p.id == id);
+        if (index != -1) {
+          pies.splice(index, 1);
+          fs.writeFile(FILE_NAME, JSON.stringify(pies), function (err) {
+            if (err) {
+              reject(err);
+            }
+            else {
+              resolve(index);
+            }
+          });
+        }
+      }
+    });
   }
 }
 
